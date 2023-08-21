ba22811fc343b0f119f75fbc278fe4d02068f960
Büşra Boyacı <busraboyaci.brb@gmail.com>
2023-08-21 09:43:27 +0300

    navigation drawer bug fixed

--- app/src/main/java/com/busra/selfcareapp/app/SelfCareApp.kt
+++ app/src/main/java/com/busra/selfcareapp/app/SelfCareApp.kt
@@ -5,21 +5,19 @@ import android.util.Log
 import androidx.compose.animation.Crossfade
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.material.Scaffold
-import androidx.compose.material.Surface
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
-import androidx.compose.ui.graphics.Color
+import androidx.navigation.NavHostController
 import androidx.navigation.compose.rememberNavController
 import com.busra.selfcareapp.bottombar.BottomNavGraph
-import com.busra.selfcareapp.data.LoginViewModel
 import com.busra.selfcareapp.navigate.ObserveScreenChanges
 import com.busra.selfcareapp.navigate.Screen
 import com.busra.selfcareapp.navigate.SelfCareAppRouter
+import com.busra.selfcareapp.navigate.SelfCareAppRouter.currentScreen
 import com.busra.selfcareapp.screens.AddHabitScreen
 import com.busra.selfcareapp.screens.BottomBar
 import com.busra.selfcareapp.screens.HomeScreen
 import com.busra.selfcareapp.screens.LoginScreen
-import com.busra.selfcareapp.screens.MainScreen
 import com.busra.selfcareapp.screens.ProfileScreen
 import com.busra.selfcareapp.screens.SettingsScreen
 import com.busra.selfcareapp.screens.SignUpScreen
@@ -30,14 +28,20 @@ import com.busra.selfcareapp.screens.TermsAndConditionScreen
 fun SelfCareApp() {
 //    surface is a container
     val navController = rememberNavController()
+    val currentScreen = SelfCareAppRouter.currentScreen.value // Access the current screen value
 
     Scaffold(
         modifier = Modifier.fillMaxSize(),
-        bottomBar = { BottomBar(navController = navController) }
-
+        bottomBar = {
+            if (currentScreen is Screen.HomeScreen ||
+                currentScreen is Screen.ProfileScreen ||
+                currentScreen is Screen.SettingsScreen
+            ) {
+                BottomBar(navController = navController)
+            }
+        }
     ) {
         ObserveScreenChanges()
-        BottomNavGraph(navController)
         Crossfade(targetState = SelfCareAppRouter.currentScreen, label = "") { currentState ->
             println("currentState.value SkinCareApp: " + currentState.value)
             when (currentState.value) {
@@ -57,8 +61,7 @@ fun SelfCareApp() {
 
                 is Screen.HomeScreen -> {
                     Log.d("HomeScreen", "HomeScreen()")
-                    HomeScreen()
-
+                    HomeScreenWithBottomNav(navController)
                 }
 
                 is Screen.AddHabitScreen -> {
@@ -68,15 +71,33 @@ fun SelfCareApp() {
 
                 is Screen.SettingsScreen -> {
                     Log.d("SettingsScreen", "SettingsScreen()")
-                    SettingsScreen()
+                    SettingsScreenWithBottomNav(navController)
                 }
 
                 is Screen.ProfileScreen -> {
                     Log.d("ProfileScreen", "ProfileScreen()")
-                    ProfileScreen()
+                    ProfileScreenWithBottomNav(navController)
                 }
             }
         }
-
     }
+}
+
+@SuppressLint("UnusedMaterialScaffoldPaddingParameter")
+@Composable
+fun HomeScreenWithBottomNav(navController: NavHostController) {
+    HomeScreen()
+    BottomNavGraph(navController)
+}
+
+@Composable
+fun SettingsScreenWithBottomNav(navController: NavHostController) {
+    SettingsScreen()
+    BottomNavGraph(navController)
+}
+
+@Composable
+fun ProfileScreenWithBottomNav(navController: NavHostController) {
+    ProfileScreen()
+    BottomNavGraph(navController)
 }
