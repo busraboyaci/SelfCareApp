18ccc31d564bd011db09aecce55f17f224dff158
Büşra Boyacı <busraboyaci.brb@gmail.com>
2023-08-28 17:13:04 +0300

    EditHabitScreenTopRow added to EditHabitScreen for design

--- app/src/main/java/com/busra/selfcareapp/screens/EditHabitScreen.kt
+++ app/src/main/java/com/busra/selfcareapp/screens/EditHabitScreen.kt
@@ -2,6 +2,7 @@ package com.busra.selfcareapp.screens
 
 import androidx.compose.foundation.background
 import androidx.compose.foundation.layout.Column
+import androidx.compose.foundation.layout.fillMaxHeight
 import androidx.compose.foundation.layout.fillMaxSize
 import androidx.compose.foundation.layout.fillMaxWidth
 import androidx.compose.foundation.layout.padding
@@ -15,6 +16,7 @@ import androidx.compose.ui.unit.dp
 import androidx.compose.ui.unit.sp
 import com.busra.selfcareapp.HabitState
 import com.busra.selfcareapp.R
+import com.busra.selfcareapp.components.EditHabitScreenTopRow
 import com.busra.selfcareapp.data.roomdb.HabitDbModel
 import com.busra.selfcareapp.navigate.Screen
 import com.busra.selfcareapp.navigate.SelfCareAppRouter
@@ -29,17 +31,17 @@ fun EditHabitScreen(selectedHabit: HabitDbModel) {
             .fillMaxSize()
             .background(colorResource(id = R.color.cream))
     ) {
+//        column  ->
         Column(
             modifier = Modifier
                 .fillMaxWidth()
-                .padding(16.dp)
+                .fillMaxHeight()
+                .background(colorResource(id = R.color.cream))
+                .padding(top = 10.dp)
         ) {
-            Text(
-                text = "Edit Habit",
-                fontSize = 20.sp,
-                fontWeight = FontWeight.Bold,
-                modifier = Modifier.padding(bottom = 16.dp)
-            )
+            EditHabitScreenTopRow(onButtonClicked = {
+                SelfCareAppRouter.navigateTo(Screen.AddHabitScreen)
+            })
 
             // Seçili habit'a ait bilgileri Text olarak göster
             Text(text = "Habit Name: ${selectedHabit.habitName}")
